a034cbffc7da402a40121878c83cb72770b21992
veenx0704 <veenx2020@gmail.com>
2024-11-06 22:56:52 -0800

    Added SideMenu

---
+++ frontend/src/components/SideMenu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from '../styles/SideMenu.module.css';
+
+const SideMenu = () => {
+    return (
+        <aside className={styles.sideMenu}>
+            <div className={styles.name}>
+                taiki tsukahara
+            </div>
+            <nav className={styles.menu}>
+                <ul>
+                    <li className={styles.menuItem}>about me</li>
+                    <li className={styles.menuItem}>personal projects</li>
+                    <li className={styles.menuItem}>academic projects</li>
+                    <li className={styles.menuItem}>skills</li>
+                    <li className={styles.menuItem}>resume</li>
+                    <li className={styles.menuItem}>contact me</li>
+                </ul>
+            </nav>
+            <div className={styles.divider}></div> {/* Divider Element */}
+        </aside>
+    );
+};
+
+export default SideMenu;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideMenu from '../components/SideMenu';
+import styles from '../styles/HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <div className={styles.homepageContainer}> 
+            <SideMenu />
+            <main className={styles.mainContent}> 
+                {/* Home page content*/}
+                <p>testing...</p>
+            </main>
+        </div>
+    );
+};
+
+export default HomePage;
