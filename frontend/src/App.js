052d3f6286745c9adf48344dc716672295ef3122
veenx0704 <veenx2020@gmail.com>
2024-11-07 00:00:09 -0800

    Updated frontend configuration and dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutMe from './pages/AboutMe';
 
-
 function App() {
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutMe />} />
-          </Routes>
-      </Router>
+    <Router basename={process.env.PUBLIC_URL}> {/* basename here */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutMe />} />
+      </Routes>
+    </Router>
   );
 }
 
